b9b6e1713ebe959d177cc18cad03c683908a056d
Ahart Powers <apowersiii@me.com>
2020-10-21 21:52:15 +0900

    Initial Commit

---
+++ MenardCal/MenardCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenardCalApp.swift
+//  MenardCal
+//
+//  Created by AhartIII on 2020/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MenardCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
